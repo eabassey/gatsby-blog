7aab1b22624c0f368be1da4511f2d71b2015e760
Enoch Abassey <eabassey@yahoo.com>
2018-11-26 15:48:40 +0200

    initial comm

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Enoch Abassey',
+    description: 'Architecture, Devops, Every line of code',
+    siteUrl: 'https://gatsby-blog.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://gatsby-blog.netlify.com',
       },
     },
     'gatsby-plugin-emotion',
--- src/website-config.ts
+++ src/website-config.ts
@@ -24,14 +24,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Enoch Kujem Abassey',
+  description: 'Architecture, Devops, Every line of code',
   coverImage: 'img/blog-cover.jpg',
-  logo: 'img/ghost-logo.png',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
+  logo: '',
+  siteUrl: 'https://gatsby-blog.netlify.com',
+  facebook: 'https://www.facebook.com/enochabassey',
+  twitter: 'https://twitter.com/enochabassey',
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
 };
